89b891c7c000bd5e4be6f606802930c748e20f90
Manav0806 <manav08gupta@gmail.com>
2024-04-24 12:46:26 +0530

    Revert "tried to add paths to nav"
    This reverts commit 8adab7682587b2a6937eaff2d3f83aac650d8814.

--- venv/app.py
+++ venv/app.py
@@ -48,7 +48,6 @@ def predict():
         ph = request.form['ph']
         rainfall_In_mm = request.form['rainfall_In_mm']
 
-
         feature = np.array([[Nitrogen,P,K,temperature,humidity,ph,rainfall_In_mm]])
         
         predicted_value = rf_model.predict(feature)
